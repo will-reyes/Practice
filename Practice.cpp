669b50326e007fef3ab8d3df39a0946cdd7daa71
willi <willi@DESKTOP-IMV8MKS>
2025-01-17 11:27:14 -0600

    Add project files.

---
+++ Practice.cpp
@@ -0,0 +1,12 @@
+// Practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "This is a test to publish code to the Github repository." << endl;
+}
